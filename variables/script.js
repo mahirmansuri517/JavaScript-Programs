50d343f7e272d60451a8b6f1819a49ab76b05404
mahir <mahirmansuri517@gmail.com>
2024-10-30 14:42:23 +0530

    JS variables

---
+++ variables/script.js
@@ -0,0 +1,19 @@
+let firstName = 'Mahir'
+let lastName = 'Mansuri'
+let age = 18
+let isHappy = true
+
+
+
+
+
+
+firstName = 100
+console.log(firstName)
+
+const hourInDay = 24
+
+let userIntro = 
+    'hi, my name is '+ firstName + ' ' 
+    + lastName + ' I am ' + age + ' '
+    + 'years old.'

999530618746d8a58388bb96403a38be37fc8a45
mahir <mahirmansuri517@gmail.com>
2024-11-04 16:21:07 +0530

    method with argument

--- String Methods And Properties/script.js
+++ String Methods And Properties/script.js
@@ -1,1 +1,13 @@
-const message = 'Hello World!'
+const message = 'Hello world!'
+
+const faltuMessage = '      Hi, I am Mahir      '
+const finalMessage = faltuMessage.trim()
+const finalMessageLowerCase = faltuMessage.trim().toLowerCase()
+const replaceMessage = finalMessage.replace('Hi','Hello')
+
+const lastFourDigit = '6473'
+const maskedAccountNumber = lastFourDigit.padStart(16,'*')
+// const maskedAccountNumber = lastFourDigit.padEnd(16,'*')
+// const a = message.toUpperCase()
+
+const ascii = message.charCodeAt(6)
